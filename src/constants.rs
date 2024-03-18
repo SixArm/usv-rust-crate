ea55c0188c890ffd02f570a33d79d73d018d6252
Joel Parker Henderson <joel@joelparkerhenderson.com>
2024-03-18 17:29:59 +0000

    Add constants

---
+++ src/constants.rs
@@ -0,0 +1,29 @@
+pub const FILE_SEPARATOR_CONTROL_CHAR: char = '\u{001C}';
+pub const FILE_SEPARATOR_CONTROL_STR: &str = "\u{001C}";
+pub const FILE_SEPARATOR_SYMBOL_CHAR: char = '␜';
+pub const FILE_SEPARATOR_SYMBOL_STR: &str = "␜";
+
+pub const GROUP_SEPARATOR_CONTROL_CHAR: char = '\u{001D}';
+pub const GROUP_SEPARATOR_SYMBOL_CHAR: char = '\u{001D}';
+pub const GROUP_SEPARATOR_CONTROL_STR: &str = "␝";
+pub const GROUP_SEPARATOR_SYMBOL_STR: &str = "␝";
+
+pub const RECORD_SEPARATOR_CONTROL_CHAR: char = '\u{001E}';
+pub const RECORD_SEPARATOR_CONTROL_STR: &str = "\u{001E}";
+pub const RECORD_SEPARATOR_SYMBOL_STR: &str = "␞";
+pub const RECORD_SEPARATOR_SYMBOL_CHAR: char = '␞';
+
+pub const UNIT_SEPARATOR_CONTROL_CHAR: char = '\u{001F}';
+pub const UNIT_SEPARATOR_CONTROL_STR: &str = "\u{001F}";
+pub const UNIT_SEPARATOR_SYMBOL_CHAR: char = '␟';
+pub const UNIT_SEPARATOR_SYMBOL_STR: &str = "␟";
+
+pub const ESCAPE_CONTROL_CHAR: char = '\u{001B}';
+pub const ESCAPE_CONTROL_STR: &str = "\u{001B}";
+pub const ESCAPE_SYMBOL_CHAR: char = '␛';
+pub const ESCAPE_SYMBOL_STR: &str = "␛";
+
+pub const END_OF_TRANSMISSION_CONTROL_CHAR: char = '\u{0004}';
+pub const END_OF_TRANSMISSION_CONTROL_STR: &str = "\u{0004}";
+pub const END_OF_TRANSMISSION_SYMBOL_CHAR: char = '␄';
+pub const END_OF_TRANSMISSION_SYMBOL_STR: &str = "␄";
