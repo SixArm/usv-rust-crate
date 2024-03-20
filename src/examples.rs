28e9d6b101fd6f50fc60d2268968528a7bb6282e
Joel Parker Henderson <joel@joelparkerhenderson.com>
2024-03-20 17:02:48 +0000

    Add examples for tests

--- src/examples.rs
+++ src/examples.rs
@@ -1,61 +1,121 @@
 //! Examples of styles with USV units. These can be useful for demos and tests.
 
+/// Example Style::braces() with USV unit. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_BRACES_UNIT: &str = "a{US}";
+
 /// Example Style::braces() with USV units. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_BRACES_UNITS: &str = "a{US}b{US}";
+
+/// Example Style::braces() with USV record. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_BRACES_RECORD: &str = "a{US}b{US}{RS}";
 
 /// Example Style::braces() with USV records. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_BRACES_RECORDS: &str = "a{US}b{US}{RS}c{US}d{US}{RS}";
 
+/// Example Style::braces() with USV group. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_BRACES_GROUP: &str = "a{US}b{US}{RS}c{US}d{US}{RS}{GS}";
+
 /// Example Style::braces() with USV groups. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_BRACES_GROUPS: &str = "a{US}b{US}{RS}c{US}d{US}{RS}{GS}e{US}f{US}{RS}g{US}h{US}{RS}{GS}";
+
+/// Example Style::braces() with USV file. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_BRACES_FILE: &str = "a{US}b{US}{RS}c{US}d{US}{RS}{GS}e{US}f{US}{RS}g{US}h{US}{RS}{GS}{FS}";
 
 /// Example Style::braces() with USV files. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_BRACES_FILES: &str = "a{US}b{US}{RS}c{US}d{US}{RS}{GS}e{US}f{US}{RS}g{US}h{US}{RS}{GS}{FS}i{US}j{US}{RS}k{US}l{US}{RS}{GS}m{US}n{US}{RS}o{US}p{US}{RS}{GS}{FS}";
 
+/// Example Style::controls() with USV unit. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_CONTROLS_UNIT: &str = "a\u{001F}";
+
 /// Example Style::controls() with USV units. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_CONTROLS_UNITS: &str = "a\u{001F}b\u{001F}";
+
+/// Example Style::controls() with USV record. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_CONTROLS_RECORD: &str = "a\u{001F}b\u{001F}\u{001E}";
 
 /// Example Style::controls() with USV records. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_CONTROLS_RECORDS: &str = "a\u{001F}b\u{001F}\u{001E}c\u{001F}d\u{001F}\u{001E}";
 
+/// Example Style::controls() with USV group. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_CONTROLS_GROUP: &str = "a\u{001F}b\u{001F}\u{001E}c\u{001F}d\u{001F}\u{001E}\u{001D}";
+
 /// Example Style::controls() with USV groups. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_CONTROLS_GROUPS: &str = "a\u{001F}b\u{001F}\u{001E}c\u{001F}d\u{001F}\u{001E}\u{001D}e\u{001F}f\u{001F}\u{001E}g\u{001F}h\u{001F}\u{001E}\u{001D}";
+
+/// Example Style::controls() with USV file. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_CONTROLS_FILE: &str = "a\u{001F}b\u{001F}\u{001E}c\u{001F}d\u{001F}\u{001E}\u{001D}e\u{001F}f\u{001F}\u{001E}g\u{001F}h\u{001F}\u{001E}\u{001D}\u{001C}";
 
 /// Example Style::controls() with USV files. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_CONTROLS_FILES: &str = "a\u{001F}b\u{001F}\u{001E}c\u{001F}d\u{001F}\u{001E}\u{001D}e\u{001F}f\u{001F}\u{001E}g\u{001F}h\u{001F}\u{001E}\u{001D}\u{001C}i\u{001F}j\u{001F}\u{001E}k\u{001F}l\u{001F}\u{001E}\u{001D}m\u{001F}n\u{001F}\u{001E}o\u{001F}p\u{001F}\u{001E}\u{001D}\u{001C}";
 
+/// Example Style::symbols() with USV unit. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_SYMBOLS_UNIT: &str = "a␟";
+
 /// Example Style::symbols() with USV units. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_SYMBOLS_UNITS: &str = "a␟b␟";
+
+/// Example Style::symbols() with USV record. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_SYMBOLS_RECORD: &str = "a␟b␟␞";
 
 /// Example Style::symbols() with USV records. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_SYMBOLS_RECORDS: &str = "a␟b␟␞c␟d␟␞";
 
+/// Example Style::symbols() with USV group. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_SYMBOLS_GROUP: &str = "a␟b␟␞c␟d␟␞␝";
+
 /// Example Style::symbols() with USV groups. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_SYMBOLS_GROUPS: &str = "a␟b␟␞c␟d␟␞␝e␟f␟␞g␟h␟␞␝";
+
+/// Example Style::symbols() with USV file. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_SYMBOLS_FILE: &str = "a␟b␟␞c␟d␟␞␝e␟f␟␞g␟h␟␞␝␜";
 
 /// Example Style::symbols() with USV files. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_SYMBOLS_FILES: &str = "a␟b␟␞c␟d␟␞␝e␟f␟␞g␟h␟␞␝␜i␟j␟␞k␟l␟␞␝m␟n␟␞o␟p␟␞␝␜";
 
+/// Example Style::liners() with USV unit. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_LINERS_UNIT: &str = "a\n␟\n";
+
 /// Example Style::liners() with USV units. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_LINERS_UNITS: &str = "a\n␟\nb\n␟\n";
+
+/// Example Style::liners() with USV record. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_LINERS_RECORD: &str = "a\n␟\nb\n␟\n\n␞\n";
 
 /// Example Style::liners() with USV records. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_LINERS_RECORDS: &str = "a\n␟\nb\n␟\n\n␞\nc\n␟\nd\n␟\n\n␞\n";
 
+/// Example Style::liners() with USV group. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_LINERS_GROUP: &str = "a\n␟\nb\n␟\n\n␞\nc\n␟\nd\n␟\n\n␞\n\n␝\n";
+
 /// Example Style::liners() with USV groups. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_LINERS_GROUPS: &str = "a\n␟\nb\n␟\n\n␞\nc\n␟\nd\n␟\n\n␞\n\n␝\ne\n␟\nf\n␟\n\n␞\ng\n␟\nh\n␟\n\n␞\n\n␝\n";
+
+/// Example Style::liners() with USV file. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_LINERS_FILE: &str = "a\n␟\nb\n␟\n\n␞\nc\n␟\nd\n␟\n\n␞\n\n␝\ne\n␟\nf\n␟\n\n␞\ng\n␟\nh\n␟\n\n␞\n\n␝\n\n␜\n";
 
 /// Example Style::liners() with USV files. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_LINERS_FILES: &str = "a\n␟\nb\n␟\n\n␞\nc\n␟\nd\n␟\n\n␞\n\n␝\ne\n␟\nf\n␟\n\n␞\ng\n␟\nh\n␟\n\n␞\n\n␝\n\n␜\ni\n␟\nj\n␟\n\n␞\nk\n␟\nl\n␟\n\n␞\n\n␝\nm\n␟\nn\n␟\n\n␞\no\n␟\np\n␟\n\n␞\n\n␝\n\n␜\n";
 
+/// Example Style::sheets() with USV unit. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_SHEETS_UNIT: &str = "a␟b␟";
+
 /// Example Style::sheets() with USV units. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_SHEETS_UNITS: &str = "a␟b␟";
+
+/// Example Style::sheets() with USV record. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_SHEETS_RECORD: &str = "a␟b␟␞\n";
 
 /// Example Style::sheets() with USV records. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_SHEETS_RECORDS: &str = "a␟b␟␞\nc␟d␟␞\n";
 
+/// Example Style::sheets() with USV group. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_SHEETS_GROUP: &str = "a␟b␟␞\nc␟d␟␞\n␝\n";
+
 /// Example Style::sheets() with USV groups. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_SHEETS_GROUPS: &str = "a␟b␟␞\nc␟d␟␞\n␝\ne␟f␟␞\ng␟h␟␞\n␝\n";
+
+/// Example Style::sheets() with USV file. This can be useful for demos and tests.
+pub const EXAMPLE_STYLE_SHEETS_FILE: &str = "a␟b␟␞\nc␟d␟␞\n␝\ne␟f␟␞\ng␟h␟␞\n␝\n␜\n";
 
 /// Example Style::sheets() with USV files. This can be useful for demos and tests.
 pub const EXAMPLE_STYLE_SHEETS_FILES: &str = "a␟b␟␞\nc␟d␟␞\n␝\ne␟f␟␞\ng␟h␟␞\n␝\n␜\ni␟j␟␞\nk␟l␟␞\n␝\nm␟n␟␞\no␟p␟␞\n␝\n␜\n";
